e5793e746752f5bce449df4e3e8a1931b864a5ea
Yiwen Zhang <yiwenzhg@umich.edu>
2021-12-27 23:20:16 -0500

    [Homa] major impl will be in Channel & Host; keep HomaFlow simple

---
+++ ext/homa_flow.cpp
@@ -0,0 +1,24 @@
+#include "homa_flow.h"
+
+#include <cstdio>
+
+#include "../coresim/channel.h"
+#include "../coresim/event.h"
+#include "../coresim/packet.h"
+#include "../coresim/queue.h"
+#include "../run/params.h"
+
+extern double get_current_time();
+extern void add_to_event_queue(Event *);
+extern DCExpParams params;
+
+HomaFlow::HomaFlow(uint32_t id, double start_time, uint32_t size, Host *s, Host *d,
+    uint32_t flow_priority) : Flow(id, start_time, size, s, d, flow_priority) {}
+
+void HomaFlow::receive_data_pkt(Packet* p) {
+    p->flow->channel->receive_data_pkt(p);
+}
+
+void HomaFlow::receive_ack(uint64_t ack, std::vector<uint64_t> sack_list, double pkt_start_ts, uint32_t priority, uint32_t num_hops) {
+    channel->receive_ack(ack, this, sack_list, pkt_start_ts);
+}
---
+++ ext/homa_flow.h
@@ -0,0 +1,18 @@
+#ifndef EXT_HOMA_FLOW_H
+#define EXT_HOMA_FLOW_H
+
+#include "../coresim/flow.h"
+
+class Packet;
+
+class HomaFlow : public Flow {
+    public:
+        HomaFlow(uint32_t id, double start_time, uint32_t size, Host *s, Host *d,
+            uint32_t flow_priority);
+        void receive_data_pkt(Packet* p) override;
+        void receive_ack(uint64_t ack, std::vector<uint64_t> sack_list,
+                        double pkt_start_ts, uint32_t priority,
+                        uint32_t num_hops) override;
+};
+
+#endif  // EXT_HOMA_FLOW_H
